efb7373e6db093827146afa4440f37a0eb9460bd
zoe1991 <275738712@qq.com>
2016-12-12 22:17:09 +0800

    code push

--- ZoeLaunchScreenAnimation/AppDelegate.swift
+++ ZoeLaunchScreenAnimation/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window?.makeKeyAndVisible()
+        initAnimation()
         return true
     }
 
@@ -39,6 +40,70 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func initAnimation (){
+        let backgroupView = UIView.init(frame: (window?.bounds)!)
+        backgroupView.backgroundColor = UIColor.black
+        window?.addSubview(backgroupView)
+        let imageView = UIImageView.init(frame: CGRect(x: 0, y: 0, width: (window?.bounds.width)!, height: (window?.bounds.height)!))
+        imageView.image = getImageFromView(view: (window?.rootViewController?.view)!)
+        window?.addSubview(imageView)
+        
+        let maskLayer = CALayer()
+        maskLayer.contents = UIImage.init(named:"nike")?.cgImage
+        maskLayer.position = (window?.center)!
+        maskLayer.bounds = CGRect(x: 0, y: 0, width: 60,height: 60)
+        imageView.layer.mask = maskLayer
+        
+        let maskBackgroudView = UIView.init(frame: (imageView.bounds))
+        maskBackgroudView.backgroundColor = UIColor.white
+        imageView.addSubview(maskBackgroudView)
+        imageView.bringSubview(toFront: maskBackgroudView)
+        
+        let logoMaskAnimation = CAKeyframeAnimation.init(keyPath: "bounds")
+        logoMaskAnimation.duration = 1.0
+        
+        let initalBounds = maskLayer.bounds
+        let secondBounds = CGRect(x:0,y:0,width:30,height:30)
+        let finalBounds = CGRect(x:0,y:0,width:2000,height:2000)
+        
+        logoMaskAnimation.values = [NSValue (cgRect: initalBounds),NSValue (cgRect: secondBounds),NSValue (cgRect: finalBounds)]
+        logoMaskAnimation.keyTimes = [0,0.5,1]
+        logoMaskAnimation.timingFunctions =  [CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseInEaseOut),CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseOut),CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseOut)]
+        logoMaskAnimation.isRemovedOnCompletion = false
+        logoMaskAnimation.beginTime = CACurrentMediaTime() + 1.0
+        logoMaskAnimation.fillMode = kCAFillModeForwards
+        imageView.layer.mask?.add(logoMaskAnimation, forKey: "logoMaskAnimation")
+        
+        UIView .animate(withDuration: 0.5, delay: 1.35, options: UIViewAnimationOptions.curveEaseIn, animations: {()-> Void in
+            maskBackgroudView.alpha = 0.0;
+        }, completion: {(finished) -> Void in
+            maskBackgroudView.removeFromSuperview()
+        })
+
+        
+        UIView.animate(withDuration: 0.5, delay: 1, options: UIViewAnimationOptions.transitionCurlUp, animations: {
+            ()-> Void in
+            imageView.transform = CGAffineTransform (scaleX: 1.1, y: 1.1)
+        }, completion: {(finished) -> Void in
+            UIView .animate(withDuration: 0.3, delay: 0, options: UIViewAnimationOptions.curveEaseInOut, animations: {()-> Void in
+                imageView.transform = CGAffineTransform .identity;
+            }, completion: {(finished) -> Void in
+                imageView.removeFromSuperview()
+                backgroupView.removeFromSuperview()
+                
+            })
+        })
+        
+    }
+    
+    func getImageFromView(view:UIView) -> UIImage{
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, UIScreen.main.scale)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
     }
 
 
--- ZoeLaunchScreenAnimation/ViewController.swift
+++ ZoeLaunchScreenAnimation/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        let label = UILabel.init(frame:CGRect(x:0,y:view.frame.height/2,width:view.frame.size.width,height:100))
+        label.text = "i'm zoe"
+        label.textAlignment = .center
+        self.view.addSubview(label)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
